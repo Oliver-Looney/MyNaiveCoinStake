f7894233d1948a6aefadfd13e07790c86f6893de
Oliver-Looney <Oliver.Looney@liberty-it.co.uk>
2021-09-04 22:01:34 +0100

    added validation of blocks and blockchain

--- src/components/Blockchain.ts
+++ src/components/Blockchain.ts
@@ -8,13 +8,59 @@ const calculateHash = (index: number, previousHash: string, timestamp: number, d
 const genesisBlock: Block = new Block(
     0, '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7', null, 1465154705, 'genesisBlock');
 
+function getLatestBlock() {
+    return blockchain[blockchain.length - 1];
+}
+
 const generateNextBlock = (blockData: string) => {
     const previousBlock: Block = getLatestBlock();
     const nextIndex: number = previousBlock.index + 1;
     const nextTimestamp: number = new Date().getTime() / 1000;
     const nextHash: string = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData);
-    const newBlock: Block = new Block(nextIndex, nextHash, previousBlock.hash, nextTimestamp, blockData);
-    return newBlock;
+    return new Block(nextIndex, nextHash, previousBlock.hash, nextTimestamp, blockData);
 }
 
-const blockchain:Block[] = [genesisBlock];
+const blockchain: Block[] = [genesisBlock];
+
+function calculateHashForBlock(blockToGetHashOf: Block) {
+    return calculateHash(blockToGetHashOf.index, blockToGetHashOf.previousHash, blockToGetHashOf.timestamp, blockToGetHashOf.data);
+}
+
+const isBlockValid = (newBlock: Block, previousBlock: Block) => {
+    if (previousBlock.index + 1 !== newBlock.index) {
+        console.log("invalid index");
+        return false;
+    } else if (previousBlock.hash !== newBlock.previousHash) {
+        console.log("invalid previous hash");
+        return false;
+    } else if (calculateHashForBlock(newBlock) !== newBlock.hash) {
+        console.log(typeof (newBlock.hash) + " " + typeof calculateHashForBlock(newBlock));
+        console.log("Invalid hash: " + calculateHashForBlock(newBlock) + " " + newBlock.hash);
+        return false;
+    }
+    return isBlockStructureValid(newBlock);
+}
+
+const isBlockStructureValid = (block: Block): boolean => {
+    return typeof block.index === "number"
+        && typeof block.hash === "string"
+        && typeof block.previousHash === "string"
+        && typeof block.timestamp === "number"
+        && typeof block.data === "string";
+}
+
+const isValidChain = (blockchainToValidate:Block[]):boolean=>{
+    const isValidGenesis=(block:Block):boolean=>{
+        return JSON.stringify(block)===JSON.stringify(genesisBlock);
+    };
+    if(!isValidGenesis(blockchainToValidate[0])){
+        return false;
+    }
+    for (let i=0;i<blockchainToValidate.length;i++){
+        if(!isBlockValid(blockchainToValidate[i],blockchainToValidate[i-1])){
+            return false;
+        }
+    }
+    return true;
+}
+
